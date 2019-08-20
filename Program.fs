f80ea134e41f9d0a3acb71c82d0e4ef09756d4ce
0918nobita <nobita.0918@gmail.com>
2019-08-20 22:57:28 +0900

    🎉 Initial commit

---
+++ Program.fs
@@ -0,0 +1,9 @@
+﻿open Gtk
+
+let () =
+    Application.Init()
+    let window = new Window("My first GTK# Application")
+    window.Add(new Label("Hello, world!"))
+    window.Resize(400, 300)
+    window.ShowAll()
+    Application.Run()
